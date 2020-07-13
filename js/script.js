d670599981fbbfa330a59159a5ae1ec2adfd0154
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-07-13 00:57:25 -0300

    Ajuste no código e tornando o slide automatico

--- js/script.js
+++ js/script.js
@@ -1,26 +1,19 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slideshow-slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
+let posicao = 0;
+function showSlides() {
+  let slides = document.getElementsByClassName("slideshow-slide");
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+  }
+  posicao++;
+  if (posicao > slides.length) {
+    posicao = 1;
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[posicao - 1].style.display = "block";
+  dots[posicao - 1].className += " active";
+  setTimeout(showSlides, 2000);
 }
+showSlides();
