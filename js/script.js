157a330fe2daa19f37f74a37d178802da1530da2
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-07-13 01:46:41 -0300

    Atualização do script do slide

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-let posicao = 0;
-function showSlides() {
+let posicao = 1;
+showSlides(posicao);
+
+function currentSlide(n) {
+  showSlides((posicao = n));
+}
+function showSlides(n) {
   let slides = document.getElementsByClassName("slideshow-slide");
   let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    posicao = 1;
+  }
+  if (n < 1) {
+    posicao = slides.length;
+  }
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  posicao++;
-  if (posicao > slides.length) {
-    posicao = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
+  for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[posicao - 1].style.display = "block";
   dots[posicao - 1].className += " active";
-  setTimeout(showSlides, 2000);
 }
-showSlides();
